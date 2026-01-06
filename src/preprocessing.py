2056ce53b11208ef5929c7d06b6ae970feaa9a60
thehardikmadaan <hardik.madaan5@gmail.com>
2026-01-06 17:35:34 +0100

    Enhanced preprocessing by adding scalers (MinMaxScaler, StandardScaler) and encoders (OrdinalEncoder, OneHotEncoder).

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,4 +1,21 @@
 from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import OrdinalEncoder
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import StandardScaler
 
-#imputer
+#Imputer
 imputer = SimpleImputer(strategy="median")
+# imputer.set_output(transform="pandas")
+
+#Ordinal Encoder
+ordinal_encoder = OrdinalEncoder()
+
+#One Hot Encoder
+cat_encoder = OneHotEncoder(sparse_output=False)
+
+#Min max Scaler
+min_max_scaler = MinMaxScaler(feature_range=(-1,1))
+
+#Standard Scaler
+std_scaler = StandardScaler()
