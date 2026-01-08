ee5cbb445ae5de041cad6a566955397abc451920
thehardikmadaan <hardik.madaan5@gmail.com>
2026-01-08 15:59:21 +0100

    Added categorical pipeline with `SimpleImputer` and `OneHotEncoder`, updated comments in `pipeline.py`, and refreshed execution timestamps in the notebook.

--- src/pipeline.py
+++ src/pipeline.py
@@ -2,10 +2,17 @@
 
 from sklearn.impute import SimpleImputer
 from sklearn.pipeline import Pipeline, make_pipeline
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
 
 #First impute than Scale the input
 
 #num_pipeline = Pipeline([("impute", SimpleImputer(strategy="meidan")]),("standardize", StandardScaler())]
 num_pipeline = make_pipeline(SimpleImputer(strategy="median"), StandardScaler())
+
+#Category pipeline , converts text to numerical 1hot standard
+cat_pipeline = make_pipeline(
+    SimpleImputer(strategy= "most_frequent"),
+    OneHotEncoder(handle_unknown="ignore"),
+)
+
 
